06de6267a1dd6ec3712005532c0ba5f2882114c3
Misha En <mishaenn@gmail.com>
2020-02-07 13:19:14 +0300

    Добавил запуск приложения

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,7 +1,4 @@
 <?php
-    /*TODO:Подключение ядра*/
     require_once ROOTKERNEL.'/kernel.php';
-    /*TODO:Сборка ядра*/
     build_kernel();
-    /*TODO:Запуск приложения*/
     run($_SERVER['REQUEST_URI']);
